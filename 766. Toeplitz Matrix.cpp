83272833436ccefef7a4dea63832240f386a87a7
AkshatParihar <91876785+AkshatParihar@users.noreply.github.com>
2022-10-31 23:05:01 +0530

    Create 766. Toeplitz Matrix.cpp

---
+++ 766. Toeplitz Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int M = matrix.size();
+        if(M==0||M==1) return true;
+        int N = matrix[0].size();
+
+        for(int i=0;i<M-1;i++)
+        {
+            for(int j=0;j<N-1;j++)
+            {
+                if(matrix[i][j] != matrix[i+1][j+1])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
